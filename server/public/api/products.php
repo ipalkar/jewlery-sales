00a958053d8652a67455c4d094833cb188beb57f
Ishani Palkar <ishani.palkar@gmail.com>
2019-05-21 22:23:37 +0000

    BE01 Ishani

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,11 +1,17 @@
 <?php
 
+require_once('./products.php');
+
 header('Content-Type: application/json');
 
-if (empty($_GET['id'])) {
-  readfile('dummy-products-list.json');
-} else {
-  readfile('dummy-product-details.json');
-}
+//if (empty($_GET['id'])) {
+//  readfile('dummy-products-list.json');
+//} else {
+//  readfile('dummy-product-details.json');
+//}
+
+set_exception_handler(error_handler);
+$output = file_get_contents('products_list.json');
+print($output);
 
 ?>
