a8899de0616da3a8cde04a77733bf4aef74c1fc6
Ishani Palkar <ishani.palkar@gmail.com>
2019-05-21 22:35:39 +0000

    changed array brackets

--- server/public/api/functions.php
+++ server/public/api/functions.php
@@ -1,8 +1,8 @@
 <?php
 function error_handler($error){$output =
-    array(success =>false,
+    [success =>false,
           error => $error->getMessage()
-);
+];
 
 $json_output = json_encode($output);
 print($json_output);
