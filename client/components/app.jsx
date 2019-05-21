302207ab70729b187715d68086e70505ad6e9ba4
Ishani Palkar <ishani.palkar@gmail.com>
2019-05-21 17:51:28 +0000

    view details front-end

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from './header';
 import ProductList from './product-list';
+import ProductDetails from './product-details';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { products: [] };
+    this.state = {
+      products: [],
+      view: { name: 'catalog', params: {} }
+    };
+
+    this.setView = this.setView.bind(this);
 
   }
 
@@ -19,21 +25,31 @@ export default class App extends React.Component {
 
   }
 
+  setView(name, params) {
+
+    this.setState({ view: { name: name, params: params } });
+
+  }
+
   componentDidMount() {
     this.getAllProducts();
 
   }
   render() {
-    return (<div className = 'container'>
-      <div className = 'row'>
-        <Header></Header>
-      </div>
-      <div className = "d-flex justify-content-around">
-        <div className = 'row '>
-          <ProductList products = {this.state.products}></ProductList>
+    if (this.state.view.name === 'catalog') {
+      return (<div className = 'container'>
+        <div className = 'row'>
+          <Header></Header>
         </div>
-      </div>
-    </div>);
+        <div className = "d-flex justify-content-around">
+          <div className = 'row '>
+            <ProductList onClick = {this.setView} products = {this.state.products}></ProductList>
+          </div>
+        </div>
+      </div>);
+    } else {
+      return <ProductDetails onClick = {this.setView} params = {this.state.view.params} />;
+    }
 
   }
 
