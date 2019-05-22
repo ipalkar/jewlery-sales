ceb7f45834c8348882e0df9313456c58eac6c260
Ishani Palkar <ishani.palkar@gmail.com>
2019-05-22 00:26:28 +0000

    worked on some css

--- client/components/CartSummaryItem.jsx
+++ client/components/CartSummaryItem.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 export default class CartSummaryItem extends React.Component {
   render() {
     return (
-      <div className = ' card-container'>
-        <div className = 'card'>
-          <img className = "card-img-top" src={this.props.product.image} />
-          <div className = "card-body">
-            <h5 className="card-title">{this.props.product.name}</h5>
-            <div>${this.props.product.price}</div>
-            <p> {this.props.product.shortDescription}</p>
-          </div>
+      <div className = 'container cart-item col-8'>
+        <div className = {'col-3 cart-img'}>
+          <img src={this.props.product.image} />
+        </div>
+        <div className ={'col-4 cart-details mt-2'} >
+          <h5 >{this.props.product.name}</h5>
+          <div>${this.props.product.price}</div>
+          <p> {this.props.product.shortDescription}</p>
         </div>
       </div>);
 
