ce5fc9e66b022d3d54e1946fa70944d790a1afef
Ishani Palkar <ishani.palkar@gmail.com>
2019-07-10 00:06:53 +0000

    ran build

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -7,7 +7,11 @@ export default class Header extends React.Component {
         <h1 onClick = {() => this.props.onClick('main-page', {})} className={'main-title col-9 col-sm-9 col-lg-9 mt-3'}>
           <img className ={'logo'} src="https://pbs.twimg.com/profile_images/2153334389/Transparent_Logo.png" />
                 Wicked Sales</h1>
-        <h2 className = {' col-3  cart-icon'}><i onClick = {() => this.props.onClick('cart', {})}className="fas fa-shopping-bag "></i> {this.props.cartItemCount}</h2>
+
+        <div className = "col-3 icons">
+          <button onClick = {() => this.props.onClick('catalog', {})} className = "btn-back-details mr-4">Shop All</button>
+          <h2 className = {' cart-icon'}><i onClick = {() => this.props.onClick('cart', {})}className="fas fa-shopping-bag "></i> {this.props.cartItemCount}</h2>
+        </div>
       </div>
     );
   }
