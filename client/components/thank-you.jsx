921bf0fc4ef44ec6850a0274776c2752b3831e25
Ishani Palkar <ishani.palkar@gmail.com>
2019-07-23 02:47:29 +0000

    fixed thank you page

--- client/components/thank-you.jsx
+++ client/components/thank-you.jsx
@@ -8,14 +8,17 @@ export default class ThankYou extends React.Component {
     if (!(this.props.orderHistory.discount === null)) {
       discount = <div className={'mb-2 total-conf conf-size'}>Discount : -${this.props.orderHistory.discount}</div>;
     }
+    let confNum = Math.floor(Math.random() * 90000) + 10000;
     return (
+
       <div>
         <div className ={'pink-stripe'}></div>
         <Header onClick ={this.props.onClick} cartItemCount = {this.props.cartItemCount}></Header>
         <div className ="container order-conf">
-          <div className={'mb-2 conf-size'}>Order Confirmation</div>
+          <div className={'mb-2 conf-size conf-bold'}>Order Confirmation</div>
+          <div>Order No. : #{confNum}</div>
           <div className={'mb-2'}>Hey {this.props.orderHistory.name}, thank you for your order! We are so appreciative of your business! </div>
-          <div className ="conf-style conf-size">Items Ordered</div>
+          <div className ="conf-style conf-size conf-bold">Items Ordered</div>
           {this.props.orderHistory.cart.map(product => (
             <div key = {product.id}className={'cart-summary-item'}>
               <div className ={'cart-details-container'}>
@@ -31,15 +34,8 @@ export default class ThankYou extends React.Component {
             </div>))}
 
           <div>
-            <div className={'mb-2 total-conf conf-size'}>Subtotal : ${this.props.orderHistory.subtotal}</div>
-            <div className={'mb-2 total-conf conf-size'}>Shipping : FREE</div>
-            {discount}
-            <div className={'mb-2 total-conf conf-size'}>Total : ${this.props.orderHistory.total}</div>
-          </div>
-
-          <div>
             <div className={'ship-conf mb-2'}>
-              <div className={'mb-2 conf-size'} >Shipping information</div>
+              <div className={'mb-2 conf-size conf-bold'} >Shipping information</div>
               <div>{this.props.orderHistory.name}</div>
               <div>{this.props.orderHistory.address} </div>
               <div>{this.props.orderHistory.city}, {this.props.orderHistory.state} {this.props.orderHistory.zip}</div>
@@ -47,10 +43,17 @@ export default class ThankYou extends React.Component {
               <div>{this.props.orderHistory.email}</div>
             </div>
 
-            <div className ="payment-conf">
-              <div className={'mb-2 conf-size'}>Payment Information</div>
+            <div className ={ 'info-conf ml-4'} >
+              <div className={'mb-2 conf-size conf-bold '}>Payment Information</div>
               <div>{this.props.orderHistory.nameOnCard}</div>
-              <div>●●●●●●●●●●●●{this.props.orderHistory.cardNum.slice(-4)}</div>
+              <div>Card: ●●●●●●●●●●●●{this.props.orderHistory.cardNum.slice(-4)}</div>
+            </div>
+
+            <div className ="payment-conf">
+              <div className={'mb-2 total-conf conf-size'}>Subtotal : ${this.props.orderHistory.subtotal}</div>
+              <div className={'mb-2 total-conf conf-size'}>Shipping : FREE</div>
+              {discount}
+              <div className={'mb-2 total-conf conf-size'}>Total : ${this.props.orderHistory.total}</div>
             </div>
 
           </div>
